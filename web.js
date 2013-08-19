e85e180bc4e3a32f52e06b06c1b1e623230df3b2
Nil Gallifa <nil.gallifa@gmail.com>
2013-08-19 06:46:34 +0000

    Web.js with port 8080

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
-var fs = require('fs');
-var buf = fs.readFileSync("index.html");
 
-app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response)  {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() { 
+    console.log("Listening on "+ port);
 });
